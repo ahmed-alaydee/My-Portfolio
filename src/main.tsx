ae05a39f39fd712736b1080813d96c40b9e4b092
Ahmed-Alayde <149985057+ahmed-alaydee@users.noreply.github.com>
2026-01-13 05:33:03 -0800

    Insert JSON-LD script for personal schema
    Added structured data script for personal information.

--- src/main.tsx
+++ src/main.tsx
@@ -6,5 +6,20 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+
+    
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Person",
+  "name": "Ahmed samir ",
+  "jobTitle": "full-stack , web development",
+  "url": "https://my-portfolio-theta-six-13.vercel.app/",
+  "sameAs": [
+    "https://www.instagram.com/ahmed_ayde/",
+    "https://www.linkedin.com/in/ahmed-alayadi-3a3bb3291/",   
+  ]
+}
+</script>
   </StrictMode>
 );
